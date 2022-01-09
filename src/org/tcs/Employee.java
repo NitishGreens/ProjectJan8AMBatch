222488cd9950e0c463d92fb233ea2a2234058794
NitishGreens <nitishgreens@gmail.com>
2022-01-09 15:28:22 +0530

    task done

---
+++ src/org/tcs/Employee.java
@@ -0,0 +1,16 @@
+package org.tcs;
+
+public class Employee {
+
+	public void add() {
+		System.out.println("add");
+	}
+
+	public static void main(String[] args) {
+
+		Employee e = new Employee();
+		e.add();
+
+	}
+
+}
